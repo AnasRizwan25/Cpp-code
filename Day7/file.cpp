0337dad04730d627a3b94ef30f7b0fa009e63fe0
AnasRizwan25 <anasgame024@gmail.com>
2024-09-12 15:34:36 +0500

    Questions

--- Day7/file.cpp
+++ Day7/file.cpp
@@ -31,20 +31,65 @@ using namespace std;
 //   return 0;
 // }
 
+
+
+// int main() {
+//   int n, i;
+//   // reading value of n from user
+//   cin >> n;
+//   i = n;
+
+//   if (i > 0) {
+//     // continously divide i if it is even
+//     while (i % 2 == 0) {
+//       i = i / 2;
+//     }
+
+//     // check if n is a power of 2
+//     if (i == 1) {
+//       cout << n << " is a power of 2";
+//     } else {
+//       cout << n << " is not a power of 2";
+//     }
+
+//   } 
+
+//   else{
+//     cout << "Enter a valid positive number";
+//   }
+//   return 0;
+// }
+
+// int main(){
+  
+//   int i = 6;
+
+//   if(i>0){
+//     while(i % 2 == 0){
+//       i = i / 2;
+//     }
+
+//     if(i == 1){
+//       cout << "a power of 2";
+//     }else{
+//       cout<< "not a power of 2";
+//     }
+//   }
+//   else{
+//     cout << "Enter a valid positive number";
+//   }
+  
+//   return 0;
+// }
+
 int main(){
 
-  int n = 34;
+  int n = 6;
 
-  for(int i = 1; i <= n; i++){
-    if( n / i == i*2){
-      cout << "Power of 2";
-      break;
-    }
-    else{
-      cout << i << endl;
-      cout << "NO" << endl;
-    }
+  if((n & (n-1)) == 0 ) {
+    cout << "Power of two";
+  }else{
+    cout << "not power of two";
   }
-  
   return 0;
 }
